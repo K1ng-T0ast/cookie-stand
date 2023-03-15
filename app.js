fa57c31d43499715cc5d9fb41f80e1ac6c0c2f12
K1ng-T0ast <kylejwhite@proton.me>
2023-03-14 17:49:18 -0700

    lab 07 complete

--- app.js
+++ app.js
@@ -15,16 +15,34 @@
 const storeList = [];
 const tableBodyEl = document.getElementById('store-values');
 
-// let shopHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-// console.log(shopHours);
+let shopHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+console.log(shopHours);
 
 function Store(location, minCustomer, maxCustomer, avgCookie) {
     this.location = location;
     this.minCustomer = minCustomer;
     this.maxCustomer = maxCustomer;
     this.avgCookie = avgCookie;
+    this.cookiesPerHour = [];
     this.totalCookies = [];
 }
+
+let operatingHours = function(){
+    let headerRow = document.createElement('tr');
+    tableBodyEl.appendChild(headerRow);
+    let blankCell = document.createElement('th');
+    headerRow.appendChild(blankCell);
+
+    for (let i = 0; i < shopHours.length; i++){
+        let timeOpen = document.createElement('th');
+        timeOpen.textContent = `${shopHours[i]}`;
+        headerRow.appendChild(timeOpen);
+    }
+    let totalCell = document.createElement('th');
+    totalCell.textContent = 'Daily Location Totals';
+    headerRow.appendChild(totalCell);
+};
+operatingHours();
 
 Store.prototype.getAverageCustomers = function () {
     const range = this.maxCustomer - this.minCustomer;
@@ -33,47 +51,56 @@ Store.prototype.getAverageCustomers = function () {
 };
 
 Store.prototype.getAverageCookies = function () {
-    const cookieAverage = [];
     for (let hour = 6; hour <= 19; hour++) {
         const cookieCustomersPerHour = this.getAverageCustomers();
-        const cookiesPurchasedPerHour = Math.round(cookieCustomersPerHour * this.avgCookie);
-        this.totalCookies += cookiesPurchasedPerHour;
+        const cookiesSold = Math.round(cookieCustomersPerHour * this.avgCookie);
+        this.totalCookies += cookiesSold;
         console.log(this.totalCookies);
-        cookieAverage.push(cookiesPurchasedPerHour);
+        this.cookiesPerHour.push(cookiesSold);
         console.log(this.avgCookie);
     }
-    cookieAverage.push;
-    return cookieAverage;
 };
 
-Store.prototype.drawTable = function(){
+Store.prototype.renderTable = function(){
     const tableRow = document.createElement('tr');
     tableBodyEl.appendChild(tableRow);
-    const tableData = document.createElement('td');
-    tableData.textContent = this.getAverageCookies;
-    tableRow.appendChild(tableData);
+    const tableData1 = document.createElement('td');
+    tableData1.textContent = this.location;
+    tableRow.appendChild(tableData1);
+    let totalCookies = 0;
+    for (let i = 0; i < this.cookiesPerHour.length; i++){
+        let tableData2 = document.createElement('td');
+        tableData2.textContent = `${this.cookiesPerHour[i]}`;
+        tableRow.appendChild(tableData2);
+        totalCookies += this.cookiesPerHour[i];
+    }
+    let tableData2 = document.createElement('td');
+    tableData2.textContent = `${totalCookies}`;
+    tableRow.appendChild(tableData2);
 };
 
 let storeSeattle = new Store('Seattle', 23, 65, 6.3);
 storeSeattle.getAverageCookies();
-storeSeattle.drawTable();
+storeSeattle.renderTable();
 storeList.push(storeSeattle);
 let storeTokyo = new Store('Tokyo', 3, 24, 1.2);
 storeTokyo.getAverageCookies();
-storeTokyo.drawTable();
+storeTokyo.renderTable();
 storeList.push(storeTokyo);
 let storeDubai = new Store('Dubai', 11, 38, 3.7);
 storeDubai.getAverageCookies();
-storeDubai.drawTable();
+storeDubai.renderTable();
 storeList.push(storeDubai);
 let storeParis = new Store('Paris', 20, 38, 2.3);
 storeParis.getAverageCookies();
-storeParis.drawTable();
+storeParis.renderTable();
 storeList.push(storeParis);
 let storeLima = new Store('Lima', 2, 16, 4.6);
 storeLima.getAverageCookies();
-storeLima.drawTable();
+storeLima.renderTable();
 storeList.push(storeLima);
+let storeTotal = [];
+storeTotal.renderTable();
 
 console.log(storeList);
 for (let i = 0; i < storeList.length; i++){
